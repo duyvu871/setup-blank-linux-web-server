6c7f1400e2b291d071ff8a1ca66f462048ecb807
duyvu871 <zoocp123@gmail.com>
2024-11-13 13:47:37 +0700

    add redis conf

---
+++ install-redis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+redis_conf_path="/etc/redis/redis.conf"
+src_dir=$(pwd)/redis
+
+function setupRedis()
+{
+	echo "Setup Redis"
+	echo "------------------------------------"
+
+	sudo apt-get install -y redis-server
+
+    # redis config
+    rsync -av $src_dir/redis.conf "${redis_conf_path}/"
+
+	sudo service redis-server start
+	redis-server -v
+	echo "------------------------------------"
+	return $?
+}
+
+setupRedis
